c7cd0214e265510eff9fe594b9bc7aff51f727b7
alejandro <alejandroweil13@gmail.com>
2021-01-15 14:51:46 -0500

    Refactored PolyTreeNode#new

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,17 +1,15 @@
 class PolyTreeNode
 
-  attr_reader :parent, :children
+  attr_reader :parent, :children, :value
 
-  def initialize(parent = nil, children = [])
-    @parent = parent
-    @children = children
-  end
 
-  def value
-    
-    value = self
-    
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
   end
 
 
+# d = Node.new('d')
+#b = Node.new('b', [d]) 
 end
